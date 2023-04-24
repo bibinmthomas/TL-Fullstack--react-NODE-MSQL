7d32d1a3c3f30ce291e94f0f5f7824ac5746f5ee
Bibin Mathew Thomas <bibinmt20@gmail.com>
2023-04-25 01:40:05 +0530

    backend cors fix

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+import { BASE_URL } from "./services/helpers";
 const instance = axios.create({
   // .. where we make our configurations
   // baseURL: process.env.REACT_APP_BE_URL
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: BASE_URL,
 });
 
 export default instance;
